19a6bbd469cff70d9598afb800ad05b63b1b89ee
SnowboardTechie <bryan@snowboardteche.com>
2016-10-18 15:05:18 -0600

    Created snowboard model and migration

---
+++ db/migrate/20161018205801_create_snowboards.rb
@@ -0,0 +1,12 @@
+class CreateSnowboards < ActiveRecord::Migration[5.0]
+  def change
+    create_table :snowboards do |t|
+      t.string :brand
+      t.string :name
+      t.string :shape
+      t.integer :length
+      t.integer :user_id
+      t.integer :catagory_id
+    end
+  end
+end
