a9127cc2cf27299d9c66a8329c532506035837a8
SnowboardTechie <bryan@snowboardteche.com>
2016-10-18 16:16:13 -0600

    Setup route for /snowbaord/new and view to create new snowboard

--- app/controllers/snowboard_controller.rb
+++ app/controllers/snowboard_controller.rb
@@ -1,3 +1,11 @@
 class SnowboardController < ApplicationController
-
+  get '/snowboard/new' do
+    if session[:id].nil?
+      flash.now[:login_error] = "To create a snowboard please login"
+      erb :'application/index'
+    else
+      @user = User.find_by(id: session[:id])
+      erb :'/snowboard/new'
+    end
+  end
 end
