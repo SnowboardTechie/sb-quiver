ca97d3675b60a64caea19e27f1a77e117eefdef4
SnowboardTechie <bryan@snowboardteche.com>
2016-10-18 14:41:56 -0600

    Created user signup view and control logic

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,27 @@
 class UserController < ApplicationController
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /^[a-zA-Z]\w{3,14}$/
+
   get '/signup' do
     erb :'user/signup'
+  end
+
+  post '/signup' do
+    email_check = params[:user][:email] =~ VALID_EMAIL_REGEX
+    password_check = params[:user][:password] =~ VALID_PASSWORD_REGEX
+
+    if password_check.nil? || email_check.nil?
+      if !email_check
+        flash[:email_error] = "Invalid email address"
+      end
+      if !password_check
+        flash[:password_error] = "Invalid password"
+      end
+      redirect '/signup'
+    else
+      user = User.create(params[:user])
+      session[:id] = user.id
+      redirect '/quiver'
+    end
   end
 end
