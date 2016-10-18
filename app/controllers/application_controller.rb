120e4e4f09bd4d3f4b984a5fa1f58759943ae65a
SnowboardTechie <bryan@snowboardteche.com>
2016-10-18 14:41:06 -0600

    Added rack-flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
   configure do
     enable :sessions unless test?
     set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  use Rack::Flash
+
 
   get '/' do
     erb :'application/index'
