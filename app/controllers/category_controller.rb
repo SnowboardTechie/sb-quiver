37ab038d35dc87bef70963d6892f90c19b1388b0
SnowboardTechie <bryan@snowboardteche.com>
2016-10-18 16:18:16 -0600

    Setup route for /category/new and view to create new category

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,3 +1,11 @@
 class CategoryController < ApplicationController
-
+  get '/category/new' do
+    if session[:id].nil?
+      flash.now[:login_error] = "To create a category please login"
+      erb :'application/index'
+    else
+      @user = User.find_by(id: session[:id])
+      erb :'/category/new'
+    end
+  end
 end
