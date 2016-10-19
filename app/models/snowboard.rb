70dbfe62cff6c00fce9555d984680e6fc53af974
SnowboardTechie <bryan@snowboardteche.com>
2016-10-19 12:53:18 -0600

    Added slugable

--- app/models/snowboard.rb
+++ app/models/snowboard.rb
@@ -2,4 +2,6 @@ class Snowboard < ActiveRecord::Base
   belongs_to :user
   has_many :category_snowboards
   has_many :categories, through: :category_snowboards
+  extend Slugable::ClassMethods
+  include Slugable::InstanceMethods
 end
