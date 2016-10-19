afe725d19c6ced34233d2ef36368657d913e88dc
SnowboardTechie <bryan@snowboardteche.com>
2016-10-18 22:12:00 -0600

    Updated to use helpers

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,11 +1,10 @@
 class CategoryController < ApplicationController
   get '/category/new' do
-    if session[:id].nil?
+    if logged_in?
+      erb :'/category/new'
+    else
       flash.now[:login_error] = "To create a category please login"
       erb :'application/index'
-    else
-      @user = User.find_by(id: session[:id])
-      erb :'/category/new'
     end
   end
 end
--- app/controllers/snowboard_controller.rb
+++ app/controllers/snowboard_controller.rb
@@ -1,11 +1,10 @@
 class SnowboardController < ApplicationController
   get '/snowboard/new' do
-    if session[:id].nil?
+    if logged_in?
+      erb :'/snowboard/new'
+    else
       flash.now[:login_error] = "To create a snowboard please login"
       erb :'application/index'
-    else
-      @user = User.find_by(id: session[:id])
-      erb :'/snowboard/new'
     end
   end
 end
