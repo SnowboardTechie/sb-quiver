f357b3a5d5295241daab8cf820750c96fe502a72
SnowboardTechie <bryan@snowboardteche.com>
2016-10-19 13:49:27 -0600

    Added /snowboard/:id/edit route

--- app/controllers/snowboard_controller.rb
+++ app/controllers/snowboard_controller.rb
@@ -20,8 +20,20 @@ class SnowboardController < ApplicationController
     end
   end
 
+  get '/snowboard/:id/edit' do
+    @snowboard = Snowboard.find_by_id(params[:id])
+    if logged_in? && current_user == @snowboard.user
+      erb :'snowboard/edit'
+    else
+      flash.now[:login_error] = "To edit this snowboard please log into the correct account"
+      erb :'application/index'
+    end
+  end
+
   get '/snowboard/:id' do
     @snowboard = Snowboard.find_by_id(params[:id])
     erb :'snowboard/show'
   end
+
+
 end
