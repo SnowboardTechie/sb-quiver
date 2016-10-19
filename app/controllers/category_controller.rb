cd8eff04713cc05e7137d93c722f31d6d1192f41
SnowboardTechie <bryan@snowboardteche.com>
2016-10-19 10:33:20 -0600

    Added logic for adding new category

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -7,4 +7,19 @@ class CategoryController < ApplicationController
       erb :'application/index'
     end
   end
+
+  post '/category/new' do
+    if logged_in?
+      category = Category.new(params[:category])
+      if category.valid?
+        current_user.categories << category
+      else
+        flash.now[:category_error] = "Category name invalid please try again."
+        erb :'/category/new'
+      end
+    else
+      flash.now[:login_error] = "To create a category please login"
+      erb :'application/index'
+    end
+  end
 end
