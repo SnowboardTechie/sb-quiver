e475a2fe92937d6d44acb4e075a613c5edde15c4
SnowboardTechie <bryan@snowboardteche.com>
2016-10-19 15:16:38 -0600

    Removed the no longer being user slugable

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,4 @@ class Category < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :name
   validates_uniqueness_of :name, scope: :user_id
-  extend Slugable::ClassMethods
-  include Slugable::InstanceMethods
 end
--- app/models/snowboard.rb
+++ app/models/snowboard.rb
@@ -2,6 +2,4 @@ class Snowboard < ActiveRecord::Base
   belongs_to :user
   has_many :category_snowboards
   has_many :categories, through: :category_snowboards
-  extend Slugable::ClassMethods
-  include Slugable::InstanceMethods
 end
