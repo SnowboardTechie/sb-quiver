528334232d65d4b2f191530d7a919e521e2c024e
SnowboardTechie <bryan@snowboardteche.com>
2016-10-19 14:17:54 -0600

    Added uniqueness checking for name

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,7 @@ class Category < ActiveRecord::Base
   has_many :snowboards, through: :category_snowboards
   belongs_to :user
   validates_presence_of :name
+  validates_uniqueness_of :name, scope: :user_id
   extend Slugable::ClassMethods
   include Slugable::InstanceMethods
 end
